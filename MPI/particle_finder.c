f06d1516864eae7e386f8ee1fdfcd684b80ef894
Andrzej Klas <plgaklas@p2159.prometheus>
2022-02-16 10:50:45 +0100

    MPI added

---
+++ MPI/particle_finder.c
@@ -0,0 +1,94 @@
+#include "particle_finder.h"
+
+int main(int argc, char** argv ){
+    Point start = {.x=0.0, .y=0.0, .z=0.0};
+    read_file(in_path);
+    MPI_Init(&argc, &argv); // aby z konsoli przyjal procesy
+    MPI_Comm_size( MPI_COMM_WORLD, &size); // z inita do tej zmiennej zapisze
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank); // wskaznik na numer watku
+    printf("HELLO. I am %d out of %d ... \n", rank, size);
+  
+    //Point start = {.x=0.0, .y=0.0, .z=0.0};
+    //read_file(in_path);
+    int i;
+    int loc_c = 0;
+    int glob_c = 0;
+    start_time();
+    if(rank !=0){
+	for( i = rank - 1; i < NUM_PARTS; i += size - 1){
+		 find_neighbours(start, 100000.5, i);
+		}
+    }
+    MPI_Reduce(&loc_c, &glob_c, 1, MPI_INT, MPI_SUM, 0,  MPI_COMM_WORLD);
+    MPI_Finalize();
+    stop_time();
+    save_results(out_path);
+    
+    print_time("MPI elapsed in ");
+    return 0;
+}
+
+// calculate euclidean metric in a 3D space
+float calc_distance(Point A, Point B){
+    float distance;
+    distance = sqrt(pow((A.x - B.x),2) + pow((A.y - B.y),2) + pow((A.z - B.z),2));
+    return distance;
+}
+
+// find all particles at a given distance from the starting point
+void find_neighbours(Point start, float distance, int index ){
+    num_of_results = 0;
+       	
+    if(calc_distance(particles[index], start) < distance){
+         results[num_of_results] = particles[index];
+         num_of_results++;
+         }    
+}
+
+// read input data from file and store in particles array
+void read_file(const char* path){
+    FILE *fp;
+    char row[MAXLENGTH];
+    char header[MAXLENGTH];
+    char *token;
+    Point *particle_p = particles;
+
+    fp = fopen(path,"r");
+
+    fgets(header, MAXLENGTH, fp);
+
+    while (feof(fp) != true){
+        fgets(row, MAXLENGTH, fp);
+        //printf("Row: %s", row);
+
+        token = strtok(row, ",");
+        if(token != NULL){
+            particle_p->x = atof(token);
+        }
+
+        token = strtok(NULL, ",");
+        if(token != NULL){
+            particle_p->y = atof(token);
+        }
+
+        token = strtok(NULL, ",");
+        if(token != NULL){
+            particle_p->z = atof(token);
+            //printf("Particle: x=%f, y=%f, z=%f\n\n", particle_p->x, particle_p->y, particle_p->z);
+            particle_p++;
+        }
+    }
+}
+
+// save found neighbours to file
+void save_results(const char *path){
+    FILE *results_f;
+    results_f = fopen(path,"w");
+    for(size_t i=0; i < num_of_results; i++){
+        char result[MAXLENGTH]; 
+        sprintf(result,"Result %li: x=%f, y=%f, z=%f\n", i+1, results[i].x, results[i].y, results[i].z);
+        fputs(result, results_f);
+    }
+    fclose(results_f);
+    printf("Found %li neighbours.\n", num_of_results);
+}
---
+++ MPI/particle_finder.h
@@ -0,0 +1,33 @@
+#ifndef PARTICLE_FINDER_H
+#define PARTICLE_FINDER_H
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include "my_timers.h"
+
+#define NUM_PARTS 10000000
+#define MAXLENGTH 100
+const char* in_path = "../particles.csv";
+const char* out_path = "results.txt";
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+} Point;
+
+Point particles[NUM_PARTS];
+Point results[NUM_PARTS];
+size_t num_of_results;
+int size, rank;
+
+float calc_distance(Point A, Point B);
+void find_neighbours(Point start, float distance, int index);
+void read_file(const char* path);
+void save_results(const char *path);
+
+#endif // PARTICLE_FINDER_H
