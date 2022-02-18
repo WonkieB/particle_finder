28aafa5b13d31a6691967005f83d7e9d06bcfaef
WonkieB <llbielecki@gmail.com>
2022-02-18 01:05:03 +0100

    Clean-up done

--- MPI/particle_finder.c
+++ MPI/particle_finder.c
@@ -1,33 +1,28 @@
 #include "particle_finder.h"
    
-int glob_c = 0;
-Point **results_p;
-Point *part_results_p;
+size_t glob_c = 0;
 float *sub_x;
 float *sub_y;
 float *sub_z;
+const int root = 0;
+int rank;
+int size;
+int size_per_proc;
 
 int main(int argc, char** argv ){
     Point start = {.x=0.0, .y=0.0, .z=0.0};
     MPI_File fh;
     
-    int size;
-    int rank;
-
-    MPI_Init(&argc, &argv); // aby z konsoli przyjal procesy
-    MPI_Comm_size( MPI_COMM_WORLD, &size); // z inita do tej zmiennej zapisze
-    MPI_Comm_rank( MPI_COMM_WORLD, &rank); // wskaznik na numer watku
-    //printf("HELLO. I am %d out of %d ... \n", rank, size);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size( MPI_COMM_WORLD, &size); // total processes number
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank); // current process index
     
-    //Point start = {.x=0.0, .y=0.0, .z=0.0};
-    //read_file(in_path);
     int i;
-    int root = 0;
-    int loc_c = 0;
-    int size_per_proc = NUM_PARTS/size;
+    size_per_proc = NUM_PARTS/size;
     sub_x = malloc(sizeof(float)*(size_per_proc));
     sub_y = malloc(sizeof(float)*(size_per_proc));
     sub_z = malloc(sizeof(float)*(size_per_proc));
+    
     if(rank == root){
         read_file(in_path);
     }
@@ -35,45 +30,33 @@ int main(int argc, char** argv ){
     MPI_Scatter(particles_x, size_per_proc, MPI_FLOAT, sub_x, size_per_proc, MPI_FLOAT, root, MPI_COMM_WORLD);
     MPI_Scatter(particles_y, size_per_proc, MPI_FLOAT, sub_y, size_per_proc, MPI_FLOAT, root, MPI_COMM_WORLD);
     MPI_Scatter(particles_z, size_per_proc, MPI_FLOAT, sub_z, size_per_proc, MPI_FLOAT, root, MPI_COMM_WORLD);
-        
-    start_time();
     
-    MPI_File_open(MPI_COMM_WORLD, "test.txt",MPI_MODE_CREATE | MPI_MODE_WRONLY,MPI_INFO_NULL,&fh);
+    if(rank == root){
+        start_time();
+    }
 
     num_of_results = 0;
     for( i = 0; i < size_per_proc; i++){
-        //printf("%f\n",sub_x[i]);
         find_neighbours(start, 100000.5, i);
         }
-    loc_c = num_of_results;
     
-        //FILE* f = fopen("test.txt","wb+");
-        //if(f==NULL){
-        //printf("failed to open file\n");exit(1);
-        //}
-        for (int i=0; i < 5; i++){
-            char buf[42];
-            //fprintf(f,"%d \n",i);
-            snprintf(buf,42,"%d \n",i);
-            MPI_File_write(fh,buf,strlen(buf), MPI_CHAR, NULL);
-        }
-        //        fclose(f);
-        MPI_File_close(&fh);
-    //printf("Hello, from %i \n", rank);
     MPI_Barrier(MPI_COMM_WORLD);
-    MPI_Reduce(&loc_c, &glob_c, 1, MPI_INT, MPI_SUM, root,  MPI_COMM_WORLD);
-    
-    save_results(out_path);
-    if(rank == 0){
+    MPI_Reduce(&num_of_results, &glob_c, 1, MPI_INT, MPI_SUM, root,  MPI_COMM_WORLD);
+    if(rank == root){
       	stop_time();
+    }
+
+    save_results(out_path);
+
+    if(rank == root){
       	print_time("MPI elapsed in ");
     }
+
     free(sub_x);
     free(sub_y);
     free(sub_z);
     MPI_Finalize();
 
-    
     return 0;
 }
 
@@ -110,7 +93,6 @@ void read_file(const char* path){
 
     while (feof(fp) != true){
         fgets(row, MAXLENGTH, fp);
-        //printf("Row: %s", row);
 
         token = strtok(row, ",");
         if(token != NULL){
@@ -135,21 +117,15 @@ void read_file(const char* path){
 // save found neighbours to file
 void save_results(const char *path){
     MPI_File results_f;
-    size_t glob_counter = 1;
-    remove(path);
-    //results_f = fopen(path,"w+");
     MPI_File_open(MPI_COMM_WORLD, path,MPI_MODE_CREATE | MPI_MODE_WRONLY,MPI_INFO_NULL,&results_f);
 
     for(size_t i=0; i < num_of_results; i++){
-        char result[MAXLENGTH]; 
-        sprintf(result,"Result %li: x=%f, y=%f, z=%f\n", glob_counter++, results[i].x, results[i].y, results[i].z);
-        //fputs(result, results_f);
-        MPI_File_write_shared(results_f,result,strlen(result), MPI_CHAR, NULL);
+            char result[MAXLENGTH]; 
+            sprintf(result,"Result: x=%f, y=%f, z=%f\n", results[i].x, results[i].y, results[i].z);
+            MPI_File_write_shared(results_f,result,strlen(result), MPI_CHAR, NULL);
     }
-    
-        
-        //        fclose(f);
     MPI_File_close(&results_f);
-    //fclose(results_f);
-    printf("Found %i neighbours.\n", glob_c);
+    if(rank == root){
+        printf("Found %li neighbours.\n", glob_c);
+    }
 }
